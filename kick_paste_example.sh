e6d1a5e9d4a9fedc519ecaf05be2527dca15947a
Zuckuss <ePQ1b4gnPZlIZ4M2VmHJ@gmail.com>
2024-07-29 16:35:23 -0700

    update kick_paste_example.sh

--- kick_paste_example.sh
+++ kick_paste_example.sh
@@ -1,5 +1,5 @@
-# This file in intended to:
-## - write base ENV varibles that can be sourced by other scripts
+# This file is intended to:
+## - write base ENV variables that can be sourced by other scripts
 ## - download and source help functions
 ## - fire off initial configuration including sysctl configuration
 
@@ -13,6 +13,10 @@
 #   ├── snmp.env
 #   └── git.env
 
+
+# Max Width
+#<-------------------------------------------------------------------------------------------------->#
+
 # Variables and Prep ------------------------------------------------------------------------------
 
 GIT_PROTOCOL=""
@@ -20,7 +24,7 @@ GIT_SERVER=""
 GIT_USER=""
 
 SECURE_USER_ID="1000"
-SECURE_USER="$(id -nu "$SECURE_USER_UID" 2>/dev/null || echo "user")"
+SECURE_USER="$(id -nu "$SECURE_USER_ID" 2>/dev/null || echo "user")"
 
 BASE="/root/"
 SCRIPTS="$BASE/scripts/"
@@ -28,28 +32,28 @@ CONFIGS="$BASE/.config/"
 ENV_GLOBAL="$CONFIGS/global.env"
 ssh_path="/root/.ssh"
 
-mkdir -p $SCRIPTS $CONFIGS $ssh_path
+mkdir -p "$SCRIPTS" "$CONFIGS" "$ssh_path"
 
 apt update
 apt install wget git openssh-client -y
 
-git clone $GIT_PROTOCOL://$GIT_SERVER/$GIT_USER/Base.git $SCRIPTS/base
+git clone "$GIT_PROTOCOL://$GIT_SERVER/$GIT_USER/Base.git" "$SCRIPTS/base"
 
 # Helper script(s)
-source $SCRIPTS/base/debian/env_writer.sh
+source "$SCRIPTS/base/debian/env_writer.sh"
 
 # System Variables -------------------------------------------------------------------------------
 cat <<EOT > /etc/environment
 
 # Localization
 export TZ="America/Vancouver"
-export LANG="en_US.UTF-8" 
+export LANG="en_US.UTF-8"
 
 # Shell
 export SHELL="/bin/bash"
 export EDITOR="nano"
 
-# Modproble Blacklist
+# Modprobe Blacklist
 export MOD_BLACKLIST="/etc/modprobe.d/blacklist.conf"
 
 EOT
@@ -59,9 +63,9 @@ env_writer \
 --service 'Global' \
 --content "
 # Filepaths
-export BASE=$BASE
-export SCRIPTS=$SCRIPTS
-export CONFIGS=$CONFIGS
+export BASE=\"$BASE\"
+export SCRIPTS=\"$SCRIPTS\"
+export CONFIGS=\"$CONFIGS\"
 export LOGS=/var/log
 "
 
@@ -69,49 +73,49 @@ export LOGS=/var/log
 env_writer \
 --service 'Network' \
 --content '
-# System 
-export DOMAIN      = "lan"                        # Referenced by scripts that need to know the local or remote domain extension
-export FIREWALL    = "iptables"                   # Referenced by scripts that need to know what, if any firewall is intended to be used
-export REV_PROXY   = "172.27.0.1"                 # Local Reverse Proxy IP (if used)
+# System
+export DOMAIN="lan"                 # Referenced by scripts that need to know the local or remote domain extension
+export FIREWALL="iptables"          # Referenced by scripts that need to know what, if any firewall is intended to be used
+export REV_PROXY="172.27.0.1"       # Local Reverse Proxy IP (if used)
 
 # Trusted Subnets
-export GREEN       = "10.0.0.0/24"                # Subnet treated with high trust
-export CYAN        = "192.168.111.0/24"           # Semi-trusted Subnet
+export GREEN="10.0.0.0/24"          # Subnet treated with high trust
+export CYAN="192.168.111.0/24"      # Semi-trusted Subnet
 
 # Untrusted Subnets
-export ORANGE      = "172.27.0.0/24"              # DMZ Subnet
-export BLUE        = "192.168.0.0/24"             # Guest Subnet
+export ORANGE="172.27.0.0/24"       # DMZ Subnet
+export BLUE="192.168.0.0/24"        # Guest Subnet
 
 # Isolated Subnets
-export BROWN       = "192.168.7.0/24"             # Isolated Subnet
+export BROWN="192.168.7.0/24"       # Isolated Subnet
 
 # VPN Subnets
-export VPN         = "10.2.0.0/24"                # Subnet for VPN clients, similar to GREEN
-export BLACK       = "172.27.7.0/24"              # VPN outbound international
-export GRAY        = "172.27.9.0/24"              # VPN outbound national
+export VPN="10.2.0.0/24"            # Subnet for VPN clients, similar to GREEN
+export BLACK="172.27.7.0/24"        # VPN outbound international
+export GRAY="172.27.9.0/24"         # VPN outbound national
 
 # Alias for RFC1918 Local area subnets
-export RFC1918     = "192.168.0.0/16,172.16.0.0/12,10.0.0.0/8"  
+export RFC1918="192.168.0.0/16,172.16.0.0/12,10.0.0.0/8"
 '
 
 # SMTP Secrets
 env_writer \
 --service 'SMTP' \
 --content '
-export ADMIN_EMAIL  =""
-export SMTP_USER    =""
-export SMTP_PASS    =""
-export SMTP_SERVER  =""
-export SMTP_PORT    ="587"
+export ADMIN_EMAIL=""
+export SMTP_USER=""
+export SMTP_PASS=""
+export SMTP_SERVER=""
+export SMTP_PORT="587"
 '
 
 # SNMP
 env_writer \
 --service 'SNMP' \
 --content '
-export SNMP_AGENT_PORT  ="161"
-export SNMP_POLLER      =""
-export SNMP_LOCATION    =""
+export SNMP_AGENT_PORT="161"
+export SNMP_POLLER=""
+export SNMP_LOCATION=""
 '
 
 # Docker -----------------------------------------------------------------------------------------
@@ -120,17 +124,17 @@ env_writer \
 --service 'Docker' \
 --content '
 # Paths
-export DOCKER_ROOT_DIR          =/var/lib/docker
-export DOCKER_MOUNTS            ="$DOCKER_ROOT_DIR/mounts"
-export DOCKER_VOLUMES           ="$DOCKER_ROOT_DIR/volumes"
-export DOCKER_CONFIGS           ="$CONFIGS/containers"
+export DOCKER_ROOT_DIR="/var/lib/docker"
+export DOCKER_MOUNTS="$DOCKER_ROOT_DIR/mounts"
+export DOCKER_VOLUMES="$DOCKER_ROOT_DIR/volumes"
+export DOCKER_CONFIGS="$CONFIGS/containers"
 
 # Docker
-export DOCKER_REGISTRY_MIRROR   =""
+export DOCKER_REGISTRY_MIRROR=""
 
 # Portainer
-export PORTAINER_SERVER         =""
-export PORTAINER_HOST           =""
+export PORTAINER_SERVER=""
+export PORTAINER_HOST=""
 '
 
 # Restic Info
@@ -138,17 +142,17 @@ env_writer \
 --service 'Restic' \
 --content '
 # Server Details, REST: https://restic.readthedocs.io/en/latest/030_preparing_a_new_repo.html#rest-server
-export RESTIC_SERVER_URL            =""
-export RESTIC_SERVER_PORT           ="443"
-export RESTIC_SERVER_TYPE           ="Rest"
+export RESTIC_SERVER_URL=""
+export RESTIC_SERVER_PORT="443"
+export RESTIC_SERVER_TYPE="Rest"
 
 # Server User Credentials
-export RESTIC_SERVER_USER_NAME      =""
-export RESTIC_SERVER_USER_PASSWORD  =""
-export RESTIC_SERVER_BASE_REPO_URL  ="rest:https://$RESTIC_SERVER_USER_NAME:$RESTIC_SERVER_USER_PASSWORD@$RESTIC_SERVER_URL"  #/$RESTIC_SERVER_USER_NAME-$LABEL
+export RESTIC_SERVER_USER_NAME=""
+export RESTIC_SERVER_USER_PASSWORD=""
+export RESTIC_SERVER_BASE_REPO_URL="rest:https://$RESTIC_SERVER_USER_NAME:$RESTIC_SERVER_USER_PASSWORD@$RESTIC_SERVER_URL"  #/$RESTIC_SERVER_USER_NAME-$LABEL
 
 # Default Repo Password
-export RESTIC_REPO_DEFAULT_PASSWORD =""
+export RESTIC_REPO_DEFAULT_PASSWORD=""
 '
 
 # Git -----------------------------------------------------------------------------------------------
@@ -156,21 +160,21 @@ env_writer \
 --service 'GIT' \
 --content "
 # Git
-export GIT_SERVER       =$GIT_SERVER
-export GIT_USER         =$GIT_USER
+export GIT_SERVER=\"$GIT_SERVER\"
+export GIT_USER=\"$GIT_USER\"
 
 # Specific Repo SSH aliases
-export GIT_APT_URL      =git@$GIT_SERVER-Apt:/$GIT_USER
-export GIT_DOCKER_URL   =git@$GIT_SERVER-Docker:/$GIT_USER
+export GIT_APT_URL=\"git@$GIT_SERVER-Apt:/$GIT_USER\"
+export GIT_DOCKER_URL=\"git@$GIT_SERVER-Docker:/$GIT_USER\"
 "
 
 ### Identity Files
-cat <<EOT > $ssh_path/gitRepo-Apt-deploy.key
+cat <<EOT > "$ssh_path/gitRepo-Apt-deploy.key"
 -----BEGIN OPENSSH PRIVATE KEY-----
 -----END OPENSSH PRIVATE KEY-----
 EOT
 
-cat <<EOT > $ssh_path/gitRepo-Docker-deploy.key
+cat <<EOT > "$ssh_path/gitRepo-Docker-deploy.key"
 -----BEGIN OPENSSH PRIVATE KEY-----
 -----END OPENSSH PRIVATE KEY-----
 EOT
@@ -189,26 +193,26 @@ Host $GIT_SERVER-Docker
 EOT
 
 # Fix permission all all the keys and config above
-chmod -R 700 $ssh_path
+chmod -R 700 "$ssh_path"
 
-git clone $GIT_APT_URL/Apt.git $SCRIPTS/apt
+git clone "$GIT_APT_URL/Apt.git" "$SCRIPTS/apt"
 
 # Bash RC -----------------------------------------------------------------------------------------------
 cat <<EOT >> /root/.bashrc
 
 # Non-Root user, from preseed.cfg
-export SECURE_USER_UID=$SECURE_USER_ID
-export SECURE_USER=$$SECURE_USER
+export SECURE_USER_UID="$SECURE_USER_ID"
+export SECURE_USER="$SECURE_USER"
 export SECURE_USER_GROUP=users
 
 # Environmental Variables Global list
 export ENV_GLOBAL="$ENV_GLOBAL"
-source $ENV_GLOBAL
+source "$ENV_GLOBAL"
 
 # APT aliases
-aptUP=$SCRIPTS/base/debian/update.sh
-update=$SCRIPTS/base/debian/update.sh
-clean=$SCRIPTS/base/clean.sh
+aptUP="$SCRIPTS/base/debian/update.sh"
+update="$SCRIPTS/base/debian/update.sh"
+clean="$SCRIPTS/base/clean.sh"
 
 EOT
 
@@ -216,7 +220,8 @@ EOT
 . ~/.bashrc
 
 # Prep-VM
-. $SCRIPTS/base/prepVM.sh
+. "$SCRIPTS/base/prepVM.sh"
 
 # Debian Base
-. $SCRIPTS/base/up.sh
+. "$SCRIPTS/base/up.sh"
+
