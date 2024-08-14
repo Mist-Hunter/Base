a0b7fc0e620e45e41438994492552a3dd728888d
Zuckuss <ePQ1b4gnPZlIZ4M2VmHJ@gmail.com>
2024-08-14 12:27:34 -0700

    update up.sh

--- firewall/up.sh
+++ firewall/up.sh
@@ -29,6 +29,7 @@ apt install iptables ipset iprange -y
 # Populate ipsets refferenced below
 . $SCRIPTS/base/firewall/ipset_BOGONS.sh
 . $SCRIPTS/base/firewall/ipset_nameservers.sh
+. $SCRIPTS/base/firewall/ipset_ntpservers.sh
 . $SCRIPTS/base/firewall/network-up.sh
 
 # if [[ $DEV_TYPE = "armv7l" ]] || [[ $DEV_TYPE = "aarch64" ]]; then
