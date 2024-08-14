dc41c985696a36e11f4c139a467c532af61f183c
Zuckuss <ePQ1b4gnPZlIZ4M2VmHJ@gmail.com>
2024-08-14 12:50:03 -0700

    update ipset_ntpservers.sh

--- firewall/ipset_ntpservers.sh
+++ firewall/ipset_ntpservers.sh
@@ -7,35 +7,24 @@ echo "Starting NTP Servers"
 # Create the ipset if it doesn't already exist
 ipset create NTP_SERVERS hash:ip -exist
 
-# Function to add IP addresses to ipset
-add_ips_to_ipset() {
-    local ip_list=$1
-    echo "$ip_list" | while read -r ip; do
-        ipset add NTP_SERVERS "$ip" -exist
-    done
-}
-
 # Function to extract and resolve servers from the config file
-extract_and_resolve() {
-    local config_file="/etc/systemd/timesyncd.conf"
 
-    # Extract NTP and FallbackNTP servers, handling possible leading whitespace.  | tr ', ' '\n' | grep -v '^#'
-    local ntp_servers=$(grep -oP '^\s*#?\s*NTP\s*=\s*\K.*' "$config_file")
-    local fallback_ntp_servers=$(grep -oP '^\s*#?\s*FallbackNTP\s*=\s*\K.*' "$config_file")
+local config_file="/etc/systemd/timesyncd.conf"
 
-    # Combine server lists and resolve them
-    local combined_servers=$(echo "$ntp_servers" "$fallback_ntp_servers")
+# Extract NTP and FallbackNTP servers, handling possible leading whitespace.  | tr ', ' '\n' | grep -v '^#'
+local ntp_servers=$(grep -oP '^\s*#?\s*NTP\s*=\s*\K.*' "$config_file")
+local fallback_ntp_servers=$(grep -oP '^\s*#?\s*FallbackNTP\s*=\s*\K.*' "$config_file")
 
-    # Resolve domain names to IP addresses and add to ipset
-    echo "combined_servers=$combined_servers"
-    for server in $combined_servers; do
-    
-        echo "server=$server"
-        nslookup "$server" | awk '/^Address: / { print $2 }'
+# Combine server lists and resolve them
+local combined_servers=$(echo "$ntp_servers" "$fallback_ntp_servers")
 
-    done | add_ips_to_ipset
-}
+# Resolve domain names to IP addresses and add to ipset
+echo "combined_servers=$combined_servers"
+for server in $combined_servers; do
 
-# Run the extraction and resolution
-extract_and_resolve
+    echo "server=$server"
+    nslookup "$server" | awk '/^Address: / { print $2 }'
+    ipset add NTP_SERVERS "$ip" -exist
+
+done
 
