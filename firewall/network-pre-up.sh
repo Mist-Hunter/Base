6782bf2bdcf22b51576e1bc7841b82419d6eddbd
Zuckuss <ePQ1b4gnPZlIZ4M2VmHJ@gmail.com>
2024-08-13 19:26:34 -0700

    update network-pre-up.sh

--- firewall/network-pre-up.sh
+++ firewall/network-pre-up.sh
@@ -8,13 +8,13 @@ if [ "$IFACE" = "$LAN_NIC" ] || [ -n "$INVOCATION_ID" ] || [ -n "$LISTEN_PID" ];
     echo "iptables persistence, pre-up, interface $IFACE"
   fi
   
-  # Restore iptables rules
-  /sbin/iptables-restore < /etc/iptables.up.rules
-
   # Execute all scripts in the lan-nic.d directory if it exists
   LAN_NIC_DIR="/etc/network/if-pre-up.d/lan-nic.d/"  # Adjust this path to the correct directory
   if [ -d "$LAN_NIC_DIR" ]; then
     echo "Running scripts in $LAN_NIC_DIR"
     run-parts --verbose "$LAN_NIC_DIR"
   fi
+
+  # Restore iptables rules
+  /sbin/iptables-restore < /etc/iptables.up.rules
 fi
