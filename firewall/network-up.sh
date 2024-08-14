5465bf7524ab317826870d49003a679bba67b385
Zuckuss <ePQ1b4gnPZlIZ4M2VmHJ@gmail.com>
2024-08-14 11:49:33 -0700

    update network-pre-up.sh and network-up.sh

--- firewall/network-up.sh
+++ firewall/network-up.sh
@@ -1,21 +1,16 @@
 #!/bin/bash
 source $ENV_NETWORK
 
-# Debian 11 runs direct from: /etc/network/if-pre-up.d/iptables, Debian 12 via SystemD Service, $INVOCATION_ID or $LISTEN_PID = Run by SystemD
-if [ "$IFACE" = "$LAN_NIC" ] || [ -n "$INVOCATION_ID" ] || [ -n "$LISTEN_PID" ]; then
+# Idealy gateway is defined first!
+export LAN_NIC_GATEWAY=$(ip route show 0.0.0.0/0 dev $LAN_NIC | cut -d\  -f3)
+echo "Setting LAN_NIC_GATEWAY: $LAN_NIC_GATEWAY"
+sed -i "s/^export LAN_NIC_GATEWAY=.*/export LAN_NIC_GATEWAY=\"$LAN_NIC_GATEWAY\"/" "$ENV_NETWORK"
 
-  # Idealy gateway is defined first!
-  export LAN_NIC_GATEWAY=$(ip route show 0.0.0.0/0 dev $LAN_NIC | cut -d\  -f3)
-  echo "Setting LAN_NIC_GATEWAY: $LAN_NIC_GATEWAY"
-  sed -i "s/^export LAN_NIC_GATEWAY=.*/export LAN_NIC_GATEWAY=\"$LAN_NIC_GATEWAY\"/" "$ENV_NETWORK"
-  
-  # Execute all scripts in the lan-nic.d directory if it exists
-  LAN_NIC_DIR="/etc/network/if-up.d/lan-nic.d"  # Adjust this path to the correct directory
-  for script in "$LAN_NIC_DIR"/*; do
-    if [ -f "$script" ] && [ -x "$script" ]; then
-      echo "Running $script"
-      "$script"
-    fi
-  done
-  
-fi
+# Execute all scripts in the lan-nic.d directory if it exists
+LAN_NIC_DIR="/etc/network/if-up.d/lan-nic.d"  # Adjust this path to the correct directory
+for script in "$LAN_NIC_DIR"/*; do
+  if [ -f "$script" ] && [ -x "$script" ]; then
+    echo "Running $script"
+    "$script"
+  fi
+done
