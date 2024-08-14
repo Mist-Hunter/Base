2e053ef7182d2e2a0f9745a3df761e6b5963c521
Zuckuss <ePQ1b4gnPZlIZ4M2VmHJ@gmail.com>
2024-08-14 12:58:06 -0700

    update ipset_ntpservers.sh

--- firewall/ipset_ntpservers.sh
+++ firewall/ipset_ntpservers.sh
@@ -1,29 +1,27 @@
-# exit #!/bin/bash
+#!/bin/bash
+
 # Example:
 # FallbackNTP=0.debian.pool.ntp.org 1.debian.pool.ntp.org 2.debian.pool.ntp.org 3.debian.pool.ntp.org
 
 echo "Starting NTP Servers"
-
-# Create the ipset if it doesn't already exist
 ipset create NTP_SERVERS hash:ip -exist
-
-# Function to extract and resolve servers from the config file
 
 config_file="/etc/systemd/timesyncd.conf"
 
-# Extract NTP and FallbackNTP servers, handling possible leading whitespace.  | tr ', ' '\n' | grep -v '^#'
 ntp_servers=$(grep -oP '^\s*#?\s*NTP\s*=\s*\K.*' "$config_file")
 fallback_ntp_servers=$(grep -oP '^\s*#?\s*FallbackNTP\s*=\s*\K.*' "$config_file")
 
-# Combine server lists and resolve them
 combined_servers=$(echo "$ntp_servers" "$fallback_ntp_servers")
 
-# Resolve domain names to IP addresses and add to ipset
 echo "combined_servers=$combined_servers"
 for server in $combined_servers; do
     echo "server=$server"
-    ip=$(dig +short $server)
-    echo "$server = $ip"
-    ipset add NTP_SERVERS "$ip" -exist
+    ips=$(dig +short "$server")
+    echo "$server = $ips"
+    while IFS= read -r ip; do
+        if [[ $ip =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+            ipset add NTP_SERVERS "$ip" -exist
+            echo "Added $ip to NTP_SERVERS"
+        fi
+    done <<< "$ips"
 done
-
