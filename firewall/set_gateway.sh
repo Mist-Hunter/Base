7a05fccb8de41f147fe227f2c4c07de987ac86fc
Zuckuss <ePQ1b4gnPZlIZ4M2VmHJ@gmail.com>
2024-09-12 18:53:23 -0700

    update set_gateway.sh

--- firewall/set_gateway.sh
+++ firewall/set_gateway.sh
@@ -5,3 +5,4 @@ source $ENV_NETWORK
 export LAN_NIC_GATEWAY=$(ip route show 0.0.0.0/0 dev $LAN_NIC | cut -d\  -f3)
 echo "Setting LAN_NIC_GATEWAY: $LAN_NIC_GATEWAY"
 sed -i "s/^export LAN_NIC_GATEWAY=.*/export LAN_NIC_GATEWAY=\"$LAN_NIC_GATEWAY\"/" "$ENV_NETWORK"
+
