fe22f7aefa39dfc014a400a94295a0171914eb2d
Zuckuss <ePQ1b4gnPZlIZ4M2VmHJ@gmail.com>
2024-09-12 18:15:40 -0700

    update network-pre-up.sh and network-up.sh

--- firewall/network-up.sh
+++ firewall/network-up.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# NOTE having trouble with a variable ENV path be available when this is called.
 source "${ENV_GLOBAL:-/root/.config/global.env}"
 
 # NOTE: Removed Debian 11 support, this expect to be run manually via systemD service units
