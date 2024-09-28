1633ad1711d9ab2511a0515bd1d8a17d77635004
Zuckuss <ePQ1b4gnPZlIZ4M2VmHJ@gmail.com>
2024-09-28 12:53:54 -0700

    update ipset_nameservers.sh

--- firewall/ipset_nameservers.sh
+++ firewall/ipset_nameservers.sh
@@ -8,3 +8,5 @@ while read -r ip; do
 done < <(grep -oP '(?<=^nameserver\s)\S+' /etc/resolv.conf)
 
 ipset_process --label "NAME_SERVERS" --hash_type "ip" --ip_array $ip_array
+
+# FIXME ipset NAME_SERVERS exists. bash: /etc/ipset/name_servers.netset: No such file or directory
