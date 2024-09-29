52ddd3ec910689d4672c50bb540b1249122cca02
Zuckuss <ePQ1b4gnPZlIZ4M2VmHJ@gmail.com>
2024-09-29 12:10:40 -0700

    update ipset_builder.sh

--- firewall/ipset_builder.sh
+++ firewall/ipset_builder.sh
@@ -115,18 +115,15 @@ create_ipsets_from_env_fqdn() {
     for env_file in "${all_env_files[@]}"; do
         if [ -f "$env_file" ]; then
             echo "Processing file: $env_file"
-            echo "Contents of $env_file:"
-            cat "$env_file"
-            echo "End of $env_file"
             
             while IFS= read -r line; do
-                if [[ $line =~ ^([A-Z_]+FQDN)=(.+)$ ]]; then
+                if [[ $line =~ ([A-Z_]+FQDN)=(.+)$ ]]; then
                     fqdn_var="${BASH_REMATCH[1]}"
                     fqdn_value="${BASH_REMATCH[2]}"
                     ip_var="${fqdn_var%FQDN}IP"
                     
-                    # Remove any surrounding quotes from fqdn_value
-                    fqdn_value=$(echo "$fqdn_value" | tr -d '"' | tr -d "'")
+                    # Remove any surrounding quotes and leading/trailing whitespace from fqdn_value
+                    fqdn_value=$(echo "$fqdn_value" | tr -d '"' | tr -d "'" | xargs)
                     
                     echo "Processing $ip_var from $fqdn_value (found in $env_file)"
                     ip_list=$(dig +short "$fqdn_value" | tr '\n' ' ' | sed 's/ $//')
