9dae40303e3436e065146f797c12d232eff1e532
Zuckuss <ePQ1b4gnPZlIZ4M2VmHJ@gmail.com>
2024-10-06 09:29:51 -0700

    update network-pre-up.sh

--- firewall/network-pre-up.sh
+++ firewall/network-pre-up.sh
@@ -72,15 +72,19 @@ done
 echo "Checking for DHCP"
 
 # Check if the network interface is configured for DHCP in /etc/network/interfaces
-# FIXME /etc/network/interfaces doesn't exist
 # TODO is this needed? If DHCP completes prior to default drop?
-if grep -q "^iface $LAN_NIC inet dhcp" /etc/network/interfaces; then
-  echo "The NIC '$LAN_NIC' is configured for DHCP. Applying DHCP rules."
-  # Allow DHCP traffic (UDP ports 67 and 68)
-  iptables -I INPUT -p udp --sport 67 --dport 68 -m comment --comment "base, firewall, network-pre-up.sh: Allow DHCP client traffic" -j ACCEPT
-  iptables -I OUTPUT -p udp --sport 68 --dport 67 -m comment --comment "base, firewall, network-pre-up.sh: Allow DHCP server traffic" -j ACCEPT
+
+# Extract DHCP setting directly from Netplan config
+# NOTE Debian 12+ seems to store DHCP flag here /etc/netplan/90-default.yaml
+dhcp_setting=$(sed -n "/name: ${LAN_NIC%${LAN_NIC#?}}*/,/^ *$/p" /etc/netplan/90-default.yaml | grep -m1 'dhcp4:' | awk '{print $2}')
+
+if [ "$dhcp_setting" = "true" ]; then
+    echo "The NIC '$LAN_NIC' is configured for DHCP. Applying DHCP rules."
+    # Allow DHCP traffic (UDP ports 67 and 68)
+    iptables -I INPUT -p udp --sport 67 --dport 68 -m comment --comment "base, firewall, network-pre-up.sh: Allow DHCP client traffic" -j ACCEPT
+    iptables -I OUTPUT -p udp --sport 68 --dport 67 -m comment --comment "base, firewall, network-pre-up.sh: Allow DHCP server traffic" -j ACCEPT
 else
-  echo "The NIC '$LAN_NIC' is not configured for DHCP. Skipping DHCP rules."
+    echo "The NIC '$LAN_NIC' is not configured for DHCP. Skipping DHCP rules."
 fi
 
 # Restore iptables rules
