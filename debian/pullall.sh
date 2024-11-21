59ff473c4712bb40c03d0615c2ba719f013bb87f
Zuckuss <ePQ1b4gnPZlIZ4M2VmHJ@gmail.com>
2024-11-21 06:24:22 -0800

    update pullall.sh

--- debian/pullall.sh
+++ debian/pullall.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# FIXME variable remote IPs @ Github are breaking IPSet method require next line hack
+. $SCRIPTS/base/firewall/ipset_fqdn_env_to_ipset.sh
+
 find "$SCRIPTS" -type d -name ".git" -exec sh -c '
     # Iterate over each found .git directory
     for dir; do
