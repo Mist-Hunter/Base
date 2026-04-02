e9eaf8f7b5bc8e65c879e0d01563fa3192597eda
DefaultUser <defaultemail@example.com>
2026-04-02 07:50:40 -0700

    feat: add script to resolve FQDNs from environment files and update corresponding ipsets

--- firewall/ipset_fqdn_env_to_ipset.sh
+++ firewall/ipset_fqdn_env_to_ipset.sh
@@ -6,7 +6,6 @@ ENV_GLOBAL="/root/.config/global.env"
 source $ENV_GLOBAL
 
 # NOTE Troubleshoot via logs @ cat /var/log/syslog | grep if-up
-
 echo "Starting ipset manager"
 
 if ! command -v grepcidr &> /dev/null; then
